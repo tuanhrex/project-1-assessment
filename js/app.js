ba0f811b8c4e2d0adc5df05b5570ee42fd363d19
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-11-12 10:50:52 -0500

    Functions are done

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let currentValue = 1
+
+const number = document.getElementById('number');
+number.textContent = currentValue;
+
+const plus = document.getElementsByClassName('plus');
+
+
+const subtract = document.getElementsByClassName('subtract');
+
+
+document.querySelector('.plus').onclick = function() {
+    
+    var input = document.querySelector('.input').value;
+    let integer = Number(input);
+    let output = integer + currentValue
+    currentValue = output
+    if (currentValue < 0) {
+        number.style.color ='red'
+    } else {
+        number.style.color ='black'
+    }
+    input = null
+    number.textContent = currentValue
+   
+
+}
+
+
+document.querySelector('.subtract').onclick = function() {
+   
+    var input = document.querySelector('.input').value;
+    let integer = Number(input);
+    let output = currentValue - integer
+    currentValue = output
+    if (currentValue < 0) {
+        number.style.color ='red'
+    } else {
+        number.style.color ='black'
+    }
+    input = null
+    number.textContent = currentValue
+   
+
+}
